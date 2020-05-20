941624f442d3b2d109552b36f69cb0a94c3ab862
Elan Utta <elanlima19@hotmail.com>
2020-05-20 09:57:38 -0300

    Project

---
+++ src/App.test.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+
+import App from './App';
+
+Enzyme.configure({ adapter: new EnzymeAdapter() })
+
+/**
+ * Factory function to create a ShallowWrapper for the App component.
+ * @function setup
+ * @param {object} props - Component props specific to this setup 
+ * @param {object} state - Initial state for setup
+ * @return {ShallowWrapper}
+ */
+
+const setup = (props = {}, state = null) => {
+  const wrapper = shallow(<App {...props} />)
+  console.log(state)
+  if (state) wrapper.setState(state);
+  return wrapper
+}
+
+/**
+ * Return a ShallowWrapper containing node(s) with the given data-test value.
+ * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search within
+ * @param {String} val -Value of data-test attribute for search 
+ * @return {ShallowWrapper}
+ */
+
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`);
+}
+
+test('renders without error', () => {
+  const wrapper = setup()
+  const appComponent = findByTestAttr(wrapper, 'component-app')
+  expect(appComponent.length).toBe(1);
+});
+
+test('renders increment button', () => {
+  const wrapper = setup()
+  const appComponent = findByTestAttr(wrapper, 'counter-display')
+  expect(appComponent.length).toBe(1);
+
+});
+
+test('renders counter display', () => {
+  const wrapper = setup()
+  const appComponent = findByTestAttr(wrapper, 'increment-button')
+  expect(appComponent.length).toBe(1);
+});
+
+test('counter starts at 0', () => {
+  const wrapper = setup()
+  const initialCounterState = wrapper.state('counter')
+  expect(initialCounterState).toBe(0)
+
+});
+
+test('clicking button increment the counter display ', () => {
+  const counter = 7;
+  const wrapper = setup(null, { counter })
+
+  const button = findByTestAttr(wrapper, 'increment-button')
+  button.simulate('click')
+
+  const counterDisplay = findByTestAttr(wrapper, 'counter-display')
+  expect(counterDisplay.text()).toContain(counter + 1)
+})
