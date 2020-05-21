27600f1a85194b80c40b0c2325ced7700bfd8db7
ElanUtta <elanlima19@gmail.com>
2020-05-21 11:01:59 -0300

    Guessed words

---
+++ jotto/src/GuessedWords.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { findByTestAttr, checkProps } from '../test/testUtils';
+import GuessedWords from './GuessedWords';
+
+
+const defaultProps = {
+    guessedWords: [{ guessedWord: 'train', letterMatchCount: 3 }]
+};
+
+/**
+ * Factory function to create a SHallowWrapper for the congrats component
+ * @function setup
+ * @param {object} props 
+ * @returns {ShallowWrapper}
+ */
+
+const setup = (props = {}) => {
+    const setUpProps = { ...defaultProps, ...props };
+    return shallow(<GuessedWords {...setUpProps} />)
+
+}
+
+test('does not throw warning with expected props ', () => {
+    checkProps(GuessedWords, defaultProps);
+})
+
+describe('if there are no words guessed', () => {
+    let wrapper;
+    beforeEach(() => {
+        const wrapper = setup({ guessedWords: [] });
+    })
+    test('renders without error', () => {
+        const component = findByTestAttr(wrapper, 'component-guessed-words');
+        expect(component.length).toBe(1)
+    });
+
+    test('renders instructions to guess a word', () => {
+
+    })
+});
+
+describe('if there are words guessed', () => {
+
+})
+
